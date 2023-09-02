2719cc1edbfed4280cfeb3fba93f593c1e374625
zhangruiyu816 <143155325+zhangruiyu816@users.noreply.github.com>
2023-09-02 16:29:44 +0800

    Add files via upload

---
+++ elmclient/vue.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require('@vue/cli-service');
+
+module.exports = defineConfig({
+  configureWebpack: {
+    devServer: {
+      port: 8081
+    }
+  },
+  transpileDependencies: true,
+  lintOnSave: false 
+});
+
